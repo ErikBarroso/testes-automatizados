1b43e69c8c624b4f82185e1a8765cdfd781189ad
Erik Barroso <coderik1996@gmail.com>
2024-01-19 15:24:39 -0300

    separando teste criando beforeeach e afterEach

--- src/tweets/entities/tweet.entity.spec.ts
+++ src/tweets/entities/tweet.entity.spec.ts
@@ -2,25 +2,55 @@ import mongoose, { mongo } from "mongoose";
 import { Tweet, TweetSchema } from "./tweet.entity";
 
 describe("Tweet Tests", () => {
-  it("should create a tweet", () => {
-    const tweet = new Tweet({
-      content: "Hello World",
-      screen_name: "DevErik",
-    });
+  //Teste de unidade
+  describe("Tweet Class", () => {
+    it("should create a tweet", () => {
+      const tweet = new Tweet({
+        content: "Hello World",
+        screen_name: "Luiz Carlos",
+      });
 
-    expect(tweet.content).toBe("Hello World");
-    expect(tweet.screen_name).toBe("DevErik");
+      expect(tweet.content).toBe("Hello World");
+      expect(tweet.screen_name).toBe("Luiz Carlos");
+    });
   });
 
-  it("create a tweet document", async () => {
-    const conn = await mongoose.connect(
-      `mongodb://root:root@db:27017/tweets_test?authSource=admin`,
-    );
-    const TweetModel = conn.model("Tweet", TweetSchema);
-    const tweet = new TweetModel({
-      content: "Hello World",
-      screen_name: "Luiz Carlos",
+  //teste de integração - menos rapido e mais custoso que unitario
+  describe("Using MongoDB", () => {
+    let conn: mongoose.Mongoose;
+
+    beforeEach(async () => {
+      try {
+        conn = await mongoose.connect(
+          `mongodb://root:root@localhost:27017/tweets_entity_test?authSource=admin`,
+        );
+      } catch (error) {
+        console.error("Error establishing MongoDB connection:", error);
+      }
     });
-    await tweet.save();
+
+    afterEach(async () => {
+      try {
+        if (conn) {
+          await conn.disconnect();
+        }
+      } catch (error) {
+        console.error("Error disconnecting from MongoDB:", error);
+      }
+    });
+
+    it("create a tweet document", async () => {
+      const TweetModel = conn.model("Tweet", TweetSchema);
+      const tweet = new TweetModel({
+        content: "Hello World",
+        screen_name: "Luiz Carlos",
+      });
+      await tweet.save();
+
+      const tweetCreated = await TweetModel.findById(tweet._id);
+
+      expect(tweetCreated.content).toBe("Hello World");
+      expect(tweetCreated.screen_name).toBe("Luiz Carlos");
+    });
   });
 });
