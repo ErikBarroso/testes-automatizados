dd807eb034ed2529a458c53d75bcb27fd5a63091
Erik Barroso <coderik1996@gmail.com>
2024-01-19 11:29:04 -0300

    start projet

---
+++ src/tweets/entities/tweet.entity.ts
@@ -0,0 +1,16 @@
+export type TweetDocument = Tweet & Document;
+
+export type TweetProps = {
+  content: string;
+  screen_name: string;
+};
+
+export class Tweet {
+  constructor(props: TweetProps) {
+    Object.assign(this, props);
+  }
+
+  content: string;
+
+  screen_name: string;
+}
